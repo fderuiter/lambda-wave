3881f1a325f7f1d76d60b5c8f67cf38111f0d678
Frederick de Ruiter <127706008+fderuiter@users.noreply.github.com>
2026-04-04 21:52:06 -0500

    🛡️ Sentinel: [MEDIUM] Prevent UI state caching via HTTP headers (#164)

--- app/Control/WebUI.hs
+++ app/Control/WebUI.hs
@@ -37,6 +37,8 @@ httpApp _ respond = respond $
         , ("X-Content-Type-Options", "nosniff")
         , ("Content-Security-Policy", "default-src 'self'; connect-src 'self' ws: wss:; script-src 'self' 'sha256-GUYznWupE5ohSMN7U8nkzx+PLxw2mKz4g7z5VcHzG+4='; style-src 'self' 'sha256-LPHpnrOc3gyP2FMotaw0TE+lqhJ6bxdrpOeCIS4ty1o='")
         , ("Strict-Transport-Security", "max-age=31536000; includeSubDomains")
+        , ("Cache-Control", "no-store, no-cache, must-revalidate, max-age=0")
+        , ("Pragma", "no-cache")
         ]
         (fromStrict indexHtml)
 
